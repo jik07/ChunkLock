d525b7e24845432e530cd1ca15254abfaa3a9299
alexlu07 <alexlu.manhasset@gmail.com>
2022-03-09 21:37:52 -0500

    first commit

---
+++ src/main/java/me/misleaded/chunklock/Chunklock.java
@@ -0,0 +1,17 @@
+package me.misleaded.chunklock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chunklock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
